90b22776680c8fbc58f7d402a1b5e0e91f660b39
Matheus <comtextos555@gmail.com>
2025-04-07 09:51:35 -0300

    Olá

---
+++ Vector.py
@@ -0,0 +1,16 @@
+numeros = [10, 20, 30, 40, 50]
+
+# Acessando o primeiro número (índice 0)
+primeiro = numeros[0] # 10
+
+# Acessando o terceiro número (índice 2)
+terceiro = numeros[2] # 30
+
+# Acessando o último número (índice -1)
+ultimo  = numeros[-1] # 50
+
+#ATIVIDADE 01 DIA 07/04/2025
+
+cores = ["vermelho", "azul", "verde"]
+cores[1] = "roxo"
+print(cores) # Saída: ['vermelhos', 'roxo', 'verde']
