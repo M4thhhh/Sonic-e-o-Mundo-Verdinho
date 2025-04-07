eb8648f77b3e728c2728ae9c9dd2eb8e09ccfd95
Matheus <comtextos555@gmail.com>
2025-04-07 10:19:12 -0300

    Olá

--- Vector.py
+++ Vector.py
@@ -7,10 +7,20 @@ primeiro = numeros[0] # 10
 terceiro = numeros[2] # 30
 
 # Acessando o último número (índice -1)
-ultimo  = numeros[-1] # 50
+# ultimo  = numeros[-1] # 50
 
-#ATIVIDADE 01 DIA 07/04/2025
+#ATIVIDADE 01 DIA 07
 
 cores = ["vermelho", "azul", "verde"]
 cores[1] = "roxo"
 print(cores) # Saída: ['vermelhos', 'roxo', 'verde']
+
+
+#ATIVIDADE 02 DIA 07
+
+carros = ["fusca", "gol", "palio", "uno"]
+
+# Percorrendo a lista com for
+print("lista de carros:")
+for carro in carros:
+    print(carro)
